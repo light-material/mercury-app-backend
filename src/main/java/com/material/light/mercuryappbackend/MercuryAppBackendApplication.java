b80d076c6ec438a08c1c4bc2185efc7db3e0931c
DJames <davejames1109@gmail.com>
2019-09-30 18:23:49 +0800

    Initialized project

---
+++ src/main/java/com/material/light/mercuryappbackend/MercuryAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.material.light.mercuryappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercuryAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercuryAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/material/light/mercuryappbackend/MercuryAppBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.material.light.mercuryappbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MercuryAppBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
